76f91ca4dce3783ccddfe98002e919a84e1f7227
Dave Ciachin <ciachind@users.noreply.github.com>
2017-11-07 22:30:28 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ciach/guesssongquiz/Questions.java
@@ -0,0 +1,48 @@
+package com.example.ciach.guesssongquiz;
+
+
+public class Questions {
+
+    public String mQuestions[] = {
+            "Name this song!"
+    };
+
+    private String mChoices[][] = {
+            {"Time", "Satisfaction", "ActualAnswer", "Thriller"}
+
+    };
+
+    private String mCorrectAnswer[] = {
+            "ActualAnswer"
+    };
+
+    public String getQuestion(int a){
+        String question = mQuestions[a];
+        return question;
+    }
+
+    public String getChoice1(int a){
+        String choice = mChoices[a][0];
+        return choice;
+    }
+
+    public String getChoice2(int a){
+        String choice = mChoices[a][1];
+        return choice;
+    }
+
+    public String getChoice3(int a){
+        String choice = mChoices[a][2];
+        return choice;
+    }
+
+    public String getChoice4(int a){
+        String choice = mChoices[a][3];
+        return choice;
+    }
+
+    public String getCorrectAnswer(int a){
+        String answer = mCorrectAnswer[a];
+        return answer;
+    }
+}
